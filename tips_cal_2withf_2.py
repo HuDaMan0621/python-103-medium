03386455cd729430c353d4695499bbc275d712de
Ying Xuan Hu <hudaman@Yings-MacBook-Pro.local>
2020-04-02 15:09:30 -0400

    added tips_cal2withf_2.py

--- tips_cal_2withf_2.py
+++ tips_cal_2withf_2.py
@@ -1,31 +1,19 @@
-# 1. Tip Calculator
-# Your task is to write a program that calculates how much of a tip to leave at a restaurant.
+"""# Allow the ability to divide the check into a equal parts amount a number of people. 
+# The user will enter the number of people to be divided amongst. Example session:
 
-# Prompt the user for two things:
-
-# The total bill amount
-# The level of service, which can be one of the following: good, fair, or bad
-# Calculate the tip amount and the total amount (bill amount + tip amount). The tip percentage based on the level of service is based on:
-
-# good -> 20%
-# fair -> 15%
-# bad -> 10%
-
-# $ python3 tip_calc.py
+# $ python3 tip_calc2.py
 # Total bill amount? 100
 # Level of service? good
+# Split how many ways? 5
 # Tip amount: $20.00
 # Total amount: $120.00
-# $ python3 tip_calc.py
-# Total bill amount? 48
-# Level of service? bad
-# Tip amount: $4.80
-# Total amount: $52.80
+# Amount per person: $24.00
+
 # Hints:
 
 # Remember that you need to convert the input from the user input to floats instead of ints. Use the float function instead of the int function.
 # To format a float number as a dollar value, use Python's formatting syntax: "%.2f" % amount
-
+"""
 total_bill = int(input('Total Bill Amount?:$ '))
 level_service = input('Level of Service? please enter good, fair or bad ')
 good = float(.20)
@@ -33,7 +21,8 @@ fair = float (.15)
 bad = float (.10)
 while level_service == 'good':
     total_bill = (total_bill+(total_bill * good))
-    sum = f'your total bill is $ {float(total_bill)}'
+    sum = f'your total bill is $ {float(total_bill)}' 
+    # {float(total_bill)}'
     print (sum)
     break
 
@@ -49,9 +38,18 @@ while level_service == 'bad':
     print (sum)
     break
 
+dollar_value = total_bill
+print(" Total Bill = ${:.2f}".format( dollar_value ))
+
+
+# print (total_bill)
 split = int(input ('Split in how many ways? '))
-total = f' {split / sum}'
-print (total)
+
+total = (total_bill / split)
+print(" Total Bill = ${:.2f}".format( total ))
+
+
+
 #subject = f'you entered, {student_subject}' 
 # greeting = f'hi {name}!!!!!'
 
